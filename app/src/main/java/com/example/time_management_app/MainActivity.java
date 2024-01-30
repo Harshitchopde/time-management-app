454a4feeab876a49b82588091b729f89c1be4ed0
Harshit chopde <109473586+Harshitchopde@users.noreply.github.com>
2024-01-30 18:56:41 +0530

    Created date done Use response.errorBody().string() to access response All working

--- app/src/main/java/com/example/time_management_app/MainActivity.java
+++ app/src/main/java/com/example/time_management_app/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.time_management_app;
 
+import static android.service.controls.ControlsProviderService.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -7,6 +9,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 
@@ -14,11 +17,20 @@ import com.example.time_management_app.Fragments.AnalysisFragment;
 import com.example.time_management_app.Fragments.DailyFragment;
 import com.example.time_management_app.Fragments.HomeFragment;
 import com.example.time_management_app.Fragments.SettingFragment;
+import com.example.time_management_app.Interface.ApiService;
 import com.example.time_management_app.Utils.FragLoad;
+import com.example.time_management_app.Utils.RetrofitClient;
 import com.example.time_management_app.random.VollyLibrary;
 import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
@@ -28,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createTodayDate();
         frameLayout = findViewById(R.id.frame_laout);
         FragLoad.loadFrag(new HomeFragment(),frameLayout,fragmentManager,1);
         // this is for testing purpose
@@ -36,6 +49,36 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView = findViewById(R.id.bottom_navigation_menu);
 
     setBottomNavigationView();
+    }
+
+    private void createTodayDate() {
+        ApiService apiService = RetrofitClient.getApiService();
+        Call<ResponseBody> call = apiService.createDate();
+
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if(response.isSuccessful()){
+                    try {
+                        String responseBody = response.body().string();
+                        Log.e(TAG, "onResponse: "+responseBody );
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }else{
+                    try {
+                        Log.e(TAG, "onResponse:fasdkjfs "+response.errorBody().string() );
+                    } catch (IOException e) {
+                        Log.e(TAG, "onResponse: "+e.toString() );
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+            }
+        });
     }
 
     private void setBottomNavigationView() {
--- app/src/main/java/com/example/time_management_app/Utils/RetrofitClient.java
+++ app/src/main/java/com/example/time_management_app/Utils/RetrofitClient.java
@@ -4,17 +4,25 @@ import static com.example.time_management_app.Utils.Utils.BASE_URL;
 
 import com.example.time_management_app.Interface.ApiService;
 
+import java.net.CookieManager;
+
+import okhttp3.JavaNetCookieJar;
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
     private static Retrofit retrofit;
-
+    private  static  OkHttpClient okHttpClient = new OkHttpClient.Builder()
+            .cookieJar(new JavaNetCookieJar(new CookieManager()))
+            // Add other configurations
+            .build();
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
+                    .client(okHttpClient)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
